c0f8ccf9995de59720831c8457d2ebb0dfb52925
sulancheng <suchengkuaile@126.com>
2018-05-02 22:09:18 +0800

    增加java字符流的储存于读取

--- src/test/java/com/light/springboot/StudentTest.java
+++ src/test/java/com/light/springboot/StudentTest.java
@@ -1,7 +1,6 @@
 package com.light.springboot;
 
 import ch.qos.logback.classic.Logger;
-import com.light.springboot.jpa.DbResponeBean;
 import com.light.springboot.jpa.StudentJpa;
 import com.light.springboot.jpa.UserServiceImpl;
 import org.junit.Test;
@@ -11,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.io.*;
 
 /**
  * Created by Administrator
@@ -27,7 +26,51 @@ public class StudentTest {
     private final static Logger logger = (Logger) LoggerFactory.getLogger(StudentTest.class);
     @Test
     public void testmyinquery() {
-        List<DbResponeBean> dbResponeBeans = userServiceImpl.mQuerylianhcx();
-        logger.info("自己定义的数据测试service="+dbResponeBeans.toString());
+//        List<DbResponeBean> dbResponeBeans = userServiceImpl.mQuerylianhcx();
+//        logger.info("自己定义的数据测试service="+dbResponeBeans.toString());
+        fileinp();
+        fileout("沃日你打野丁你给");
+    }
+    public void fileinp(){
+        //读取  BufferedInputStream
+        BufferedReader bufferedReader = null;
+        try {
+//            FileInputStream fileInputStream = new FileInputStream("e://test.txt");
+//            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream,"GB2312"));
+            bufferedReader = new BufferedReader(new FileReader("e://test.txt"));
+            //一次读取一个字节数组
+//            byte[] bts = new byte[1024];
+            char[] bts = new char[1024];
+            StringBuffer stringBuffer = new StringBuffer();
+            int len;
+            while ((len = bufferedReader.read(bts)) != -1) {
+                String str = new String(bts,0,len);
+                stringBuffer.append(str);
+            }
+            logger.info("读取的数据"+stringBuffer.toString());
+            bufferedReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void fileout(String data){
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter("e://test.txt",true));
+            bufferedWriter.write(data+"\r\n");
+            bufferedWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (bufferedWriter!=null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
